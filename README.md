21f474362410b7bdd18ccc4cc5438c41f1c51c10
Soheil <ysoheil@gmail.com>
2014-01-24 11:59:12 -0800

    add goodies

---
+++ README.md
@@ -0,0 +1,14 @@
+## Hide Menu Bar
+
+Hide your Mac Menu Bar yes extra 25 pixels helps, think zen-mode.
+
+### Usage
+php engine.php APP_NAME_TO_HIDE_MENU_BAR_FOR [ARG]
+
+`APP_NAME_TO_HIDE_MENU_BAR_FOR` parial app name to hide (eg., term, chrome)
+`ARG` if this is missing this will be a dry-run
+  
+
+![No Menu Bar](no-menu-bar.png)
+vs.
+![With Menu Bar](menu-bar.png)
---
+++ engine.php
@@ -0,0 +1,19 @@
+<?php
+
+$plists1 = explode("\n", `ls /Applications/ | awk '{ print "/Applications/"$0"/Contents/Info.plist" }'`);
+$plists2 = explode("\n", `ls /Applications/Utilities | awk '{ print "/Applications/Utilities/"$0"/Contents/Info.plist" }'`);
+$plists = array_merge($plists1, $plists2);
+$app = isset($argv[1]) ? $argv[1] : '';
+foreach ($plists as $plist) {
+  if ($app && stripos($plist, $app) === FALSE) continue;
+  echo $plist . "\n";
+  // if 2nd arg is not set this is a dry run
+  if (!isset($argv[2])) continue;
+  if (!$plist || strpos($plist, '.DS_Store') !== FALSE || strpos($plist, '.localized') !== FALSE || strpos($plist, 'Utilities') !== FALSE) continue;
+  $c = file_get_contents($plist);
+  if (strpos($c, 'LSUIPresentationMode') !== FALSE) continue;
+  $c = preg_replace('/(plist.*?\n<dict>)/', "$0\n\t<key>LSUIPresentationMode</key>\n\t<integer>4</integer>", $c);
+  `chmod +w $plist`;
+  file_put_contents($plist, $c);
+}
+
